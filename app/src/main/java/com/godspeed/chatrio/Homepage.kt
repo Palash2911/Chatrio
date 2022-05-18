d8e8507766aded48e106c8f6b4c83219d9964b6a
Palash Rambhia <palashrambhia2@gmail.com>
2022-05-18 22:16:57 +0530

    Added RecyclerView

--- app/src/main/java/com/godspeed/chatrio/Homepage.kt
+++ app/src/main/java/com/godspeed/chatrio/Homepage.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import com.godspeed.chatrio.adapter.Useradapter
 import com.godspeed.chatrio.databinding.ActivityHomepageBinding
 import com.godspeed.chatrio.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 var binding: ActivityHomepageBinding?=null
 var db: FirebaseDatabase?=null
@@ -17,6 +21,21 @@ class Homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomepageBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+        db = FirebaseDatabase.getInstance()
+        users = ArrayList<User>()
+        useradapter = Useradapter(this, users!!)
+        db!!.reference.child("Chatters")
+            .child(FirebaseAuth.getInstance().uid!!)
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
 
         setContentView(R.layout.activity_homepage)
     }
